1f6b3c14b3fd4a4273183e3e066ff56b83ac2da3
garciadelcastillo <personal@garciadelcastillo.es>
2022-05-04 19:18:21 -0400

    Added left clicker for XP farms

--- src/Program.cs
+++ src/Program.cs
@@ -18,69 +18,74 @@ namespace minecraft_poker
         [DllImport("User32.dll")]
         static extern int SetForegroundWindow(IntPtr point);
 
-        /// <summary>
-        /// Threaded Ticker function.
-        /// </summary>
-        public static void Poker()
-        {
-            // Catch and focus the app
-            // Inspired by https://stackoverflow.com/a/15292428/1934487
-            Process[] ps = Process.GetProcessesByName("javaw");
-            Process p = ps?.FirstOrDefault();
-            if (p != null)
-            {
-                Console.WriteLine("Found a running Minecraft");
+        
+        static int Timeout;
+        static int RandomDelta;
+        static int ClickLength;
+        static readonly Random rnd = new Random();
 
-                Console.WriteLine("Bringing the app on focus...");
-                IntPtr h = p.MainWindowHandle;
-                SetForegroundWindow(h);
-
-                // Alternative to SendWait()
-                // Found on https://stackoverflow.com/a/55251816/1934487
-                InputSimulator isim = new InputSimulator();
-
-                Thread.Sleep(2000);
-                Console.WriteLine("Turning Game Menu off...");
-                isim.Keyboard.KeyPress(VirtualKeyCode.ESCAPE);
-
-                double delta;
-                int time;
-                while (true)
-                {
-                    delta = RandomDelta * rnd.NextDouble();
-                    time = Timeout + (int)(delta - 0.5 * RandomDelta);
-
-                    Thread.Sleep(time);
-                    Console.WriteLine("Poke at " + DateTime.Now.ToString());
-
-                    isim.Mouse.RightButtonDown();
-                    Thread.Sleep(ClickLength);
-                    isim.Mouse.RightButtonUp();
-
-                    delta = RandomDelta * rnd.NextDouble();
-                    time = Timeout + (int)(delta - 0.5 * RandomDelta);
-
-                    Thread.Sleep(time);
-                    Console.WriteLine("Poke at " + DateTime.Now.ToString());
-                    isim.Mouse.LeftButtonDown();
-                    Thread.Sleep(ClickLength);
-                    isim.Mouse.LeftButtonUp();
-                }
-            }
-
-        }
-
-        static int Timeout = 60000;
-        static int RandomDelta = 10000;
-        static int ClickLength = 50;
-        static Random rnd = new Random();
+        static int ClickerMode = 1;
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Minecraft Poker!");
+            Console.WriteLine("A mighy clicker app for Minecraft AFK");
+            Console.WriteLine("");
 
-            Console.WriteLine("Welcome to Minecraft Poker! An AFK app to prevent your Minecraft Realms from idling...");
+            // Choose clicker mode
+            Console.WriteLine("Which clicker mode do you need?");
+            Console.WriteLine("    (1) Left-Right clicker, useful to prevent your Minecraft Realms from idling...");
+            Console.WriteLine("    (2) Left clicker, useful for XP farms...");
+            Console.Write("Enter clicker mode: ");
 
-            Console.WriteLine($"Enter time between ticks in seconds (default {Timeout / 1000}): ");
+            string clickerModeStr = Console.ReadLine();
+            try
+            {
+                if (!clickerModeStr.Equals(""))
+                {
+                    ClickerMode = Convert.ToInt32(clickerModeStr);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Wrong input, please enter only numerical values");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("");
+
+            // The threadedFunction
+            Action threadedFunction = null;
+
+            switch (ClickerMode)
+            {
+                // LEFT-RIGHT CLICK
+                case 1:
+                    // Case specific defaults
+                    Timeout = 60000;
+                    RandomDelta = 10000;
+                    ClickLength = 50;
+
+                    threadedFunction = LeftRightClicker;
+                    break;
+
+                // LEFT-CLICK only
+                case 2:
+                    // Case specific defaults
+                    Timeout = 2000;
+                    RandomDelta = 500;
+                    ClickLength = 50;
+
+                    threadedFunction = LeftClicker;
+                    break;
+
+                default:
+                    Console.WriteLine("Unknown clicker mode, exiting...");
+                    Console.ReadKey();
+                    return;
+            }
+
+            Console.WriteLine($"Enter time between clicks in seconds (default {Timeout / 1000}): ");
             string timeoutStr = Console.ReadLine();
             try
             {
@@ -127,20 +132,113 @@ namespace minecraft_poker
                 Console.ReadKey();
                 return;
             }
+            Console.WriteLine("");
 
 
             // Threaded
-            ThreadStart threadRef = new ThreadStart(Poker);
+            ThreadStart threadRef = new ThreadStart(threadedFunction);
             Thread tickerThread = new Thread(threadRef);
             tickerThread.Start();
 
-            Console.WriteLine("Press any key to exit the Poker...");
+            // Hold the app running until user presses any key
+            //Console.WriteLine("Press any key to exit the Poker...");
             Console.ReadKey();
 
+            Console.WriteLine("Exiting poker...");
             tickerThread.Abort();
             Thread.Sleep(1000);
+        }
 
-            Console.ReadKey();
+        /// <summary>
+        /// Threaded Ticker function.
+        /// </summary>
+        public static void LeftRightClicker()
+        {
+            // Catch and focus the app
+            // Inspired by https://stackoverflow.com/a/15292428/1934487
+            Process[] ps = Process.GetProcessesByName("javaw");
+            Process p = ps?.FirstOrDefault();
+            if (p != null)
+            {
+                Console.WriteLine("Found a running Minecraft");
+
+                Console.WriteLine("Bringing the app on focus...");
+                IntPtr h = p.MainWindowHandle;
+                SetForegroundWindow(h);
+
+                // Alternative to SendWait()
+                // Found on https://stackoverflow.com/a/55251816/1934487
+                InputSimulator isim = new InputSimulator();
+
+                Thread.Sleep(2000);
+                Console.WriteLine("Turning Game Menu off...");
+                isim.Keyboard.KeyPress(VirtualKeyCode.ESCAPE);
+
+                double delta;
+                int time;
+                while (true)
+                {
+                    delta = RandomDelta * rnd.NextDouble();
+                    time = Timeout + (int)(delta - 0.5 * RandomDelta);
+                    if (time < 100) time = 100;
+                    Thread.Sleep(time);
+
+                    Console.WriteLine("Left click at " + DateTime.Now.ToString());
+                    isim.Mouse.LeftButtonDown();
+                    Thread.Sleep(ClickLength);
+                    isim.Mouse.LeftButtonUp();
+
+                    delta = RandomDelta * rnd.NextDouble();
+                    time = Timeout + (int)(delta - 0.5 * RandomDelta);
+                    Thread.Sleep(time);
+
+                    Console.WriteLine("Left click at " + DateTime.Now.ToString());
+                    isim.Mouse.RightButtonDown();
+                    Thread.Sleep(ClickLength);
+                    isim.Mouse.RightButtonUp();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Threaded Ticker function.
+        /// </summary>
+        public static void LeftClicker()
+        {
+            // Catch and focus the app
+            // Inspired by https://stackoverflow.com/a/15292428/1934487
+            Process[] ps = Process.GetProcessesByName("javaw");
+            Process p = ps?.FirstOrDefault();
+            if (p != null)
+            {
+                Console.WriteLine("Found a running Minecraft");
+
+                Console.WriteLine("Bringing the app on focus...");
+                IntPtr h = p.MainWindowHandle;
+                SetForegroundWindow(h);
+
+                // Alternative to SendWait()
+                // Found on https://stackoverflow.com/a/55251816/1934487
+                InputSimulator isim = new InputSimulator();
+
+                Thread.Sleep(2000);
+                Console.WriteLine("Turning Game Menu off...");
+                isim.Keyboard.KeyPress(VirtualKeyCode.ESCAPE);
+
+                double delta;
+                int time;
+                while (true)
+                {
+                    delta = RandomDelta * rnd.NextDouble();
+                    time = Timeout + (int)(delta - 0.5 * RandomDelta);
+                    Thread.Sleep(time);
+
+                    Console.WriteLine("Left click at " + DateTime.Now.ToString());
+                    isim.Mouse.LeftButtonDown();
+                    Thread.Sleep(ClickLength);
+                    isim.Mouse.LeftButtonUp();
+                }
+            }
 
         }
     }
